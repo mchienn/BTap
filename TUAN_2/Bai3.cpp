918d1aff9a9c34efc361cf9de971db34961ecd80
Nguyễn Minh Chiến <nmc27705@gmail.com>
2024-03-13 23:52:33 +0700

    Create Bai3.cpp

---
+++ TUAN_2/Bai3.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = n; i > 0; i--)
+    {
+        for (int j = n - i; j > 0; j --)
+        {
+            cout << " ";
+        }
+        for (int j = i; j > 0; j--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
